c3ac31e3c89d843c912ba3900c4b171595fc63c8
NGUYỄN THỌ TUẤN <1712878@student.hcmus.edu.vn>
2019-03-18 15:04:02 +0700

    Revert "ds"
    This reverts commit 718a7ee504ff67b770978cf1324ccaf6ebcc123e.

--- KTMT_PROJECT1/KTMT_PROJECT1/main.cpp
+++ KTMT_PROJECT1/KTMT_PROJECT1/main.cpp
@@ -1,12 +1,10 @@
 #include "Qint.h"
 
-int main()
+void main()
 {
 	QInt a;
 	string s = "238914365438435789436554346";
 	a.SetDataDec(s);
 	a.PrintQIntBit();
 	system("pause");
-	cout << "oc cho is real" << endl;
-	return 0;
 }
