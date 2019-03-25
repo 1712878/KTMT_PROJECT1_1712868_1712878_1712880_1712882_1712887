337b21398058a61ac7dcf7fbd2cf1da8b3c7c032
NGUYỄN THỌ TUẤN <1712878@student.hcmus.edu.vn>
2019-03-25 12:32:13 +0700

    Update process file

--- KTMT_PROJECT1/KTMT_PROJECT1/main.cpp
+++ KTMT_PROJECT1/KTMT_PROJECT1/main.cpp
@@ -1,8 +1,29 @@
 #include "QInt.h"
 #include "QFloat.h"
 
-int main()
+int main(int argc, char* argv[])
 {
- 	system("pause");
+	string FileIn, FileOut;
+	int type;
+	if (argc < 4)
+	{
+		cin >> FileIn;
+		cin >> FileOut;
+		cin >> type;
+	}
+	else
+	{
+		FileIn = argv[1];
+		FileOut = argv[2];
+		type = argv[3][0];
+	}
+	ifstream input;
+	ofstream output;
+	input.open(FileIn);
+	output.open(FileOut);
+	FileProcess(input, output, type);
+	input.close();
+	output.close();
+	system("pause");
 	return 0;
 }
