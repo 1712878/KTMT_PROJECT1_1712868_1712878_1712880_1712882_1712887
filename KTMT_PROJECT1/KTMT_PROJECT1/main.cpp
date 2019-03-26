7e28b2a2ee7d71bb90eb4da243d8bc25af313cf7
NGUYỄN THANH TÙNG <1712882@student.hcmus.edu.vn>
2019-03-26 15:11:52 +0700

    sa

--- KTMT_PROJECT1/KTMT_PROJECT1/main.cpp
+++ KTMT_PROJECT1/KTMT_PROJECT1/main.cpp
@@ -1,33 +1,21 @@
 #include "QInt.h"
 #include "QFloat.h"
 
-//int main(int argc, char* argv[])
-//{
-//	string FileIn, FileOut;
-//	char type;
-//	FileIn = argv[1];
-//	FileOut = argv[2];
-//	type = argv[3][0];
-//
-//	ifstream input;
-//	ofstream output;
-//	input.open(FileIn);
-//	output.open(FileOut);
-//	FileProcess(input, output, type);
-//	input.close();
-//	output.close();
-//	system("pause");
-//	return 0;
-//}
-int main()
+int main(int argc, char* argv[])
 {
-	QInt a, b,c;
-	a.ScanQInt();
-	b.ScanQInt();
-	c = a ^ b;
-	a.PrintQIntBin();cout << endl;
-	b.PrintQIntBin();cout << endl;
-	(c).PrintQIntBin();
+	string FileIn, FileOut;
+	char type;
+	FileIn = argv[1];
+	FileOut = argv[2];
+	type = argv[3][0];
+
+	ifstream input;
+	ofstream output;
+	input.open(FileIn);
+	output.open(FileOut);
+	FileProcess(input, output, type);
+	input.close();
+	output.close();
 	system("pause");
 	return 0;
 }
